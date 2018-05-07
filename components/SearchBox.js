979d0ba54d4267ab71b78eb1ad57068f76e1412a
Ana <ana.osouz@gmail.com>
2018-05-07 12:03:23 -0400

    Add files via upload

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
-		<div className='pa2'>
+		<div className='pa5'>
 		<input 
-			className='pa3 ba b--green bg-lighest-blue'
+			className='pa3 ba b--pink bg-lighest-blue'
 			type='search' 
 			placeholder='Search Robots' 
 			onChange={searchChange}
